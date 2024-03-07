159aa67b441c85f5e2a4e6054be51fff51d48bf1
Amir Raza <amirrazadepalpur@gmail.com>
2024-03-07 15:47:51 -0800

    new commit

--- utility.js
+++ utility.js
@@ -1,12 +1,12 @@
 
 
-const name1 ="amir raza"
+const name ="amir raza"
 const age =25;
 
 const new1 = "hello "
 
 function name (){
-    console.log(`name is : ${name1}`)
+    console.log("name")
 }
 
 name();
