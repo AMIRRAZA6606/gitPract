69bb44b929e419c97d82cb14b7937a50b2cf0946
Amir Raza <amirrazadepalpur@gmail.com>
2024-03-07 15:39:15 -0800

    replace concate by templates staring

--- utility.js
+++ utility.js
@@ -1,12 +1,12 @@
 
 
-const name ="amir raza"
+const name1 ="amir raza"
 const age =25;
 
 const new1 = "hello "
 
 function name (){
-    console.log("name")
+    console.log(`name is : ${name1}`)
 }
 
 name();
